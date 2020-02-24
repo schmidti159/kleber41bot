fc7aaaa09b0402a4558f6647a8f990665c99df07
Daniel Schmidt <daniel-git@ad-schmidt.de>
2020-02-24 20:50:46 +0100

    send messages about upcoming events automatically

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 const telegraf = require('telegraf');
 const ical = require('node-ical');
 const dateFormat = require('dateformat');
+const cron = require('node-cron');
 
 const bot = new telegraf(process.env.BOT_TOKEN);
 
@@ -16,10 +17,11 @@ const events/* : event[]*/ = ical.sync.parseFile('input/Leerungstermine50655.ics
 //console.log(events);
 const today = +new Date();
 const weekMilliseconds = 7 * 24 * 60 * 60 * 1000;
+const dayMilliseconds = 24 * 60 * 60 * 1000;
 const nextWeekEvents = Object.values(events)
-                        .filter((e) => {
-                            return (e["start"] - today) < weekMilliseconds;
-                        })
+        .filter((e) => {
+            return (e["start"] - today) < weekMilliseconds;
+        })
 bot.command('week', (ctx) => {
     for(const i in nextWeekEvents) {
         const event = nextWeekEvents[i];
@@ -27,7 +29,27 @@ bot.command('week', (ctx) => {
         ctx.reply(msg);
     }
 }); 
-bot.startPolling()
+const chatIds = new Set();
+
+cron.schedule('0 16,18 * * *', () => {
+    chatIds.forEach((chatId) => {
+        const nextDayEvents = Object.values(events)
+                .filter((e) => {
+                    return (e["start"] - today) < dayMilliseconds;
+                });
+        for(const i in nextDayEvents) {
+            const event = nextDayEvents[i];
+            const msg = event["summary"]["val"] + ": " + dateFormat(event["start"], 'ddd. dd.mm.yyyy');
+            bot.telegram.sendMessage(chatId, msg);
+            console.log("msg sent: "+msg);
+        }
+    });
+});
+
+bot.command('subscribe', (ctx) => {
+    chatIds.add(ctx.chat.id);
+    console.log("subscribe with chatId: "+ctx.chat.id+" chatIds: ("+Array.from(chatIds).join(", ")+")");
+});
 
 
 bot.launch();
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/bash
 
 source setEnv.sh
 npm install
