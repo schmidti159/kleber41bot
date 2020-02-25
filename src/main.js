67f21667e1af58ac43b4cad680d1092536bf09e5
Daniel Schmidt <daniel-git@ad-schmidt.de>
2020-02-25 22:05:18 +0100

    fix handling of events of the current day

---
+++ src/main.js
@@ -0,0 +1,70 @@
+'use strict';
+// external config
+require('dotenv').config()
+// dependencies
+const telegraf = require('telegraf');
+const ical = require('node-ical');
+const dateFormat = require('dateformat');
+const cron = require('node-cron');
+// constants
+const weekMilliseconds = 7 * 24 * 60 * 60 * 1000;
+const dayMilliseconds = 24 * 60 * 60 * 1000;
+
+// get the events
+const events = ical.sync.parseFile('input/Leerungstermine50655.ics');
+
+// create the bot
+const bot = new telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('I will remind you to take out the trash the day before. Features: Use /week to display the events of the next week.'));
+bot.help((ctx) => ctx.reply('Nothing to configure.'));
+
+// the week command will display all the events of the next week
+bot.command('week', (ctx) => {
+    ctx.reply("Events in the next week:");
+    const now = +new Date();
+    const nextWeekEvents = Object.values(events)
+        .filter((e) => {
+            const isInTheNextWeek = (e.start - now) < weekMilliseconds;
+            const isToday = dateFormat(e.start, 'dd.mm.yyyy') == dateFormat(now, 'dd.mm.yyyy');
+            return isInTheNextWeek && !isToday;
+        })
+    for(const i in nextWeekEvents) {
+        const event = nextWeekEvents[i];
+        const msg = event["summary"]["val"] + ": " + dateFormat(event["start"], 'ddd. dd.mm.yyyy');
+        ctx.reply(msg);
+    }
+}); 
+
+// the subscribe command will store the chatId
+// TODO do not use a global variable but some kind of persistent storage for this
+const chatIds = new Set();
+
+bot.command('subscribe', (ctx) => {
+    chatIds.add(ctx.chat.id);
+    console.log("subscribe with chatId: "+ctx.chat.id+" chatIds: ("+Array.from(chatIds).join(", ")+")");
+    ctx.reply("I will notify you the day before.");
+});
+
+// periodically (twice a day) send a message with all upcoming events to all subscribers
+cron.schedule('0 16,18 * * *', () => {
+//cron.schedule('* * * * *', () => {
+    const now = +new Date();
+    chatIds.forEach((chatId) => {
+        const nextDayEvents = Object.values(events)
+            .filter((e) => {
+                const isInTheNext24Hours = (e.start - now) < dayMilliseconds;
+                const isToday = dateFormat(e.start, 'dd.mm.yyyy') == dateFormat(now, 'dd.mm.yyyy');
+                return isInTheNext24Hours && !isToday;
+            });
+        for(const i in nextDayEvents) {
+            const event = nextDayEvents[i];
+            const msg = event.summary.val + ": " + dateFormat(event.start, "ddd. dd.mm.yyyy");
+            bot.telegram.sendMessage(chatId, msg);
+            console.log("msg sent: "+msg);
+        }
+    });
+});
+
+
+
+bot.launch();
