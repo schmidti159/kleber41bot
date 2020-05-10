0b5b237c7cbc86a9c567c4898b63a0f0b9c1fa66
Daniel Schmidt <daniel-git@ad-schmidt.de>
2020-05-10 22:44:36 +0200

    remove test cron-expression and fix date offset (was increased for testing)

--- src/cron.js
+++ src/cron.js
@@ -7,12 +7,12 @@ import dateUtils from './utils/date.js';
 const cron = {
     launch: function (bot, events, chatIds) {
         // periodically (twice a day) send a message with all upcoming events to all subscribers
-//        nodeCron.schedule('0 16,18 * * *', () => {
-        nodeCron.schedule('* * * * *', () => {
+        nodeCron.schedule('0 16,18 * * *', () => {
+//        nodeCron.schedule('* * * * *', () => {
             chatIds.forEach((chatId) => {
                 const nextDayEvents = Object.values(events)
                     .filter((e) => {
-                        return dateUtils.eventIsInTheNextDays(e, 4);
+                        return dateUtils.eventIsInTheNextDays(e, 1);
                     });
                 for(const i in nextDayEvents) {
                     const event = nextDayEvents[i];
